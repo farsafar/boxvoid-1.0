a7e013d2cbb8d0625a54764b241f8897f654dbf7
farsafar <109830682+farsafar@users.noreply.github.com>
2022-07-22 18:47:23 -0400

    Add files via upload
    hello, to use this program you need to install python3

---
+++ boxvoid.py
@@ -0,0 +1,40 @@
+print("Bienvenido a BoxVoid")
+print("Cual es tu nombre")
+nombre = input ()
+print("hola " + nombre)
+si = True
+no = False
+
+print("Quieres Sumar Algunos Numeros")
+
+
+anwser = input().lower
+
+if anwser() == "si":
+    
+    print ("Dime el primer numero")
+    numero1 = int(input())
+    print ("Dime el segundo numero")
+    numero2 = int(input())
+    print ("el resultado")
+    print (numero1 + numero2)
+    
+if anwser () == "no":
+    
+    print ("Quieres escuchar musica")
+
+anwser1 = input().lower
+
+if anwser1() == "si":
+    
+    print("Dont Stop Me Now -- Queen. link: https://www.youtube.com/watch?v=hBp4dgE7Bho")
+    
+    
+    
+    print("Master of Puppets -- Metallica. link: https://www.youtube.com/watch?v=S7blkui3nQc")
+    
+    
+    
+    print("I'm Still Standing -- Elton John. link: https://www.youtube.com/watch?v=ZHwVBirqD2s")
+   
+print("Perdon por el poco contenido en este asistente es mi primer programa en python")
