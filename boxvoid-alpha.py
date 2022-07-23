c89dcca17b401c933771b8b9fb865a0cc9a0faf1
farsafar <109830682+farsafar@users.noreply.github.com>
2022-07-22 22:54:09 -0400

    Add files via upload

---
+++ boxvoid-alpha.py
@@ -0,0 +1,24 @@
+print("Bienvenido a BoxVoid")
+print("Cual es tu nombre")
+nombre = input ()
+print("hola " + nombre)
+
+
+
+anwser = input().lower
+
+if anwser() == "sumar":
+    
+    print ("Dime el primer numero")
+    numero1 = int(input())
+    print ("Dime el segundo numero")
+    numero2 = int(input())
+    print ("el resultado")
+    print (numero1 + numero2)
+
+if anwser () == "musica":
+    
+    print("Dont Stop Me Now -- Queen. link: https://www.youtube.com/watch?v=hBp4dgE7Bho")   
+    print("Master of Puppets -- Metallica. link: https://www.youtube.com/watch?v=S7blkui3nQc")
+    print("I'm Still Standing -- Elton John. link: https://www.youtube.com/watch?v=ZHwVBirqD2s")
+
