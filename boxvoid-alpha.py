710ff6227134ab54d3bdc8cf30d1955524746bc9
farsafar <109830682+farsafar@users.noreply.github.com>
2022-07-24 16:08:48 -0400

    Add files via upload
    Nueva actualizacion
    Cambios
    se añadieron los siguientes comandos:
    /google = busquedas de google
    /sumar = suma
    /resta = resta
    /dividir = division
    /multiplicar = multiplicacion
    /musica = escuchar musica
    /youtube = a ver videos de youtube

--- boxvoid-alpha.py
+++ boxvoid-alpha.py
@@ -1,13 +1,15 @@
+from googlesearch import search
+from ytmusicapi import YTMusic
+import pywhatkit
+
 print("Bienvenido a BoxVoid")
 print("Cual es tu nombre")
 nombre = input ()
 print("hola " + nombre)
 
-
-
 anwser = input().lower
 
-if anwser() == "sumar":
+if anwser() == "/sumar":
     
     print ("Dime el primer numero")
     numero1 = int(input())
@@ -16,9 +18,53 @@ if anwser() == "sumar":
     print ("el resultado")
     print (numero1 + numero2)
 
-if anwser () == "musica":
+if anwser() == "/resta":
     
-    print("Dont Stop Me Now -- Queen. link: https://www.youtube.com/watch?v=hBp4dgE7Bho")   
-    print("Master of Puppets -- Metallica. link: https://www.youtube.com/watch?v=S7blkui3nQc")
-    print("I'm Still Standing -- Elton John. link: https://www.youtube.com/watch?v=ZHwVBirqD2s")
+    print ("Dime el primer numero")
+    numero1 = int(input())
+    print ("Dime el segundo numero")
+    numero2 = int(input())
+    print ("el resultado")
+    print (numero1 - numero2)
 
+if anwser() == "/multiplicacion":
+    
+    print ("Dime el primer numero")
+    numero1 = int(input())
+    print ("Dime el segundo numero")
+    numero2 = int(input())
+    print ("el resultado")
+    print (numero1 * numero2)
+
+if anwser() == "/division":
+    
+    print ("Dime el primer numero")
+    numero1 = int(input())
+    print ("Dime el segundo numero")
+    numero2 = int(input())
+    print ("el resultado")
+    print (numero1 / numero2)
+
+if anwser() == "/google":
+    
+    print("Que quieres buscar ")
+    busqueda = input()
+    pywhatkit.search(busqueda)
+
+
+
+    
+
+        
+
+if anwser() == "/musica":
+
+    print("Que musica quieres escuchar")
+    musica = input()
+    pywhatkit.playonyt(musica)
+
+if anwser() == "/youtube":
+
+    print("Que Quieres ver")
+    videos = input()
+    pywhatkit.playonyt(videos)
